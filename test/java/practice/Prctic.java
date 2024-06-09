46b71c4e3e3972462b5544527af0a24f0c49682b
achegavebh96 <achegavebh96@gmail.com>
2024-06-09 15:22:37 +0530

    my 2nd commit

---
+++ test/java/practice/Prctic.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prctic {
+
+	public static void main(String[] args) throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.meesho.com/");
+	driver.manage().window().minimize();
+	Thread.sleep(2000);
+	driver.manage().window().maximize();
+	Thread.sleep(2000);
+	}
+}
